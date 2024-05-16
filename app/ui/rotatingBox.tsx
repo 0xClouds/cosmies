67e2713104507bb9ed459b0034712c0e98197fac
Namdar Mesri <namdarmesri@Namdars-Laptop.lan>
2024-05-16 12:56:43 -0700

    rotating box with numbers

--- app/ui/rotatingBox.tsx
+++ app/ui/rotatingBox.tsx
@@ -6,12 +6,12 @@ const RotatingBox = () => {
     <div className={styles.container}>
       <h1 className={styles.header}>Welcome to the lobby</h1>
       <div className={styles.cube}>
-        <div className={`${styles.face} ${styles.front}`}></div>
-        <div className={`${styles.face} ${styles.back}`}></div>
-        <div className={`${styles.face} ${styles.left}`}></div>
-        <div className={`${styles.face} ${styles.right}`}></div>
-        <div className={`${styles.face} ${styles.top}`}></div>
-        <div className={`${styles.face} ${styles.bottom}`}></div>
+        <div className={`${styles.face} ${styles.front}`}>1</div>
+        <div className={`${styles.face} ${styles.back}`}>2</div>
+        <div className={`${styles.face} ${styles.left}`}>3</div>
+        <div className={`${styles.face} ${styles.right}`}>4</div>
+        <div className={`${styles.face} ${styles.top}`}>5</div>
+        <div className={`${styles.face} ${styles.bottom}`}>6</div>
       </div>
       <h3 className={styles.footer}>Finding opponent for you</h3>
     </div>
